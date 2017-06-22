4a223c7bc1e5444d6c327190ee7f1f68bf420037
IbrahimHout <trest.best@gmail.com>
2017-06-22 18:09:37 +0300

    first Edit  adding textviews , adding some strings , colors and styles to the text views

--- app/src/main/java/com/example/zezoom/arabicapp/MainActivity.java
+++ app/src/main/java/com/example/zezoom/arabicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zezoom.arabicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void openLettersActivity(View view){
+        Intent intet = new Intent(this,LettersActivity.class);
+        startActivity(intet);
+    }
+    public void openColorsActivity(View view){
+        Intent intet = new Intent(this,ColorsActivity.class);
+        startActivity(intet);
+    }
+    public void openNumbersActivity(View view){
+        Intent intet = new Intent(this,NumbersActivity.class);
+        startActivity(intet);
+    }
+    public void openPhrasesActivity(View view){
+        Intent intet = new Intent(this,PhrasesActivity.class);
+        startActivity(intet);
     }
 }
